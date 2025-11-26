675fe973362ebe6f30a5286735f0c341f46211d4
Maximiliano Alzogaray <maxialzogaray700@gmail.com>
2025-11-26 18:19:59 -0300

    ultimo retoque

--- proyecto_final/settings.py
+++ proyecto_final/settings.py
@@ -79,9 +79,9 @@ if 'RENDER' in os.environ:
     EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
     EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
     EMAIL_USE_TLS = True
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
-    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', '')
+    EMAIL_HOST_USER = os.environ.get('maxialzogaray700@gmail.com')
+    EMAIL_HOST_PASSWORD = os.environ.get('oma gbms qrwj pjf')
+    DEFAULT_FROM_EMAIL = os.environ.get('maxialzogaray700@gmail.com')
     
 else:
     # Configuración para desarrollo
